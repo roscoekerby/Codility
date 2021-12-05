cf3fb4e36b2ed3ac44a0bcad63e8e56f2cd0b851
roscoekerby <56516234+roscoekerby@users.noreply.github.com>
2021-12-05 12:45:25 +0200

    Create BinaryGap.py

---
+++ BinaryGap.py
@@ -0,0 +1,26 @@
+def solution(N):    
+    binary = bin(N)[2:]
+    longestBinaryGap = 0
+    newGap = 0
+    i = 0
+    
+    while i < len(binary)-1:
+        if binary[i] == '1' and binary[i+1] == '0':            
+            newGap = 1
+            x = i+1
+            
+            while x < len(binary)-1:
+                x = x+1
+                
+                if (binary[x] == '0'):
+                    newGap = newGap+1                    
+                elif (binary[x] == '1'):
+                    newGap = newGap
+                    
+                    if (longestBinaryGap < newGap):
+                        longestBinaryGap = newGap                        
+                        newGap = 1                        
+                    break
+        i = i+1
+
+    return longestBinaryGap
