4d698737d61120804332b876cdcf3ffc8937bfbd
roscoekerby <56516234+roscoekerby@users.noreply.github.com>
2021-12-10 14:46:18 +0200

    Update cyclicArray.py

--- cyclicArray.py
+++ cyclicArray.py
@@ -2,9 +2,12 @@ def solution(A, K):
     Anew = A
     lenA = len(A)
     
-    for i in range(K):
-        Alast = Anew[lenA-1]
-        Anew = Anew[0:lenA-1]
-        Anew.insert(0, Alast)
+    if lenA > 0:
+        for i in range(K):
+            Alast = Anew[lenA-1]
+            Anew = Anew[0:lenA-1]
+            Anew.insert(0, Alast)
+    else:
+        Anew = []
         
     return Anew
