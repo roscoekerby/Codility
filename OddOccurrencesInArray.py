ccb1dd805fcb18219d666e16d3575449720c6b3c
roscoekerby <56516234+roscoekerby@users.noreply.github.com>
2021-12-10 22:05:29 +0200

    Update OddOccurrencesInArray.py

--- OddOccurrencesInArray.py
+++ OddOccurrencesInArray.py
@@ -1,8 +1,15 @@
 def solution(A):
-    A.sort()    
-    x = A[len(A)-1]
-    for i in range(len(A)-1):
-        if A[i] != A[i+1] and i%2 == 0:
-            x = A[i]
-        
-    return x
+    B = A.copy()
+    B.sort()
+    
+    if len(B) > 1:
+        for i in range(len(B)-1):
+            if B[i] != B[i+1] and i%2 ==0:
+                return B[i]
+            elif i == len(B)-2:
+                return B[i+1]                
+    else:
+        return B[0]     
+          
+    
+    
