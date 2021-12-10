5135b53b6923a0f0f94d89b64075cde225a72733
roscoekerby <56516234+roscoekerby@users.noreply.github.com>
2021-12-10 15:10:06 +0200

    Update x.py

--- x.py
+++ x.py
@@ -2,7 +2,7 @@ def solution(A):
     A.sort()    
     x = 0
     for i in range(len(A)-1):
-        if A[i] != A[i+1]:
+        if A[i] != A[i+1] and i%2 == 0:
             x = i
         
     return A[x]
